b757338e03d3be81fc2988004898653eb7c8480c
wu-medoc <58807721+wu-medoc@users.noreply.github.com>
2020-05-04 15:05:11 +0800

    css update

--- js/index.js
+++ js/index.js
@@ -538,4 +538,9 @@ function backLayerOne() {
     oneLayer.length=0;
 };
 
-//$('.iconpage').css('-webkit-animation-name','slideOutRight').css('animation-name','slideOutRight');
+// var u = navigator.userAgent;
+// var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+// var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+// alert('是否是Android：'+isAndroid);
+// alert('是否是iOS：'+isiOS);
+// alert('是PC：'+u);
